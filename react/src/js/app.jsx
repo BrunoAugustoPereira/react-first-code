ba6ea93e3e8f766c2994094d5e3caff2c3df232f
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2018-03-15 11:19:09 -0300

    Hello world in react

---
+++ react/src/js/app.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+let app = (
+
+  <div className="container">
+    <h1>Ola Mundo</h1>
+  </div>
+);
+
+ReactDOM.render(app,document.getElementById('app'));
