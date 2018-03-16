359cbda05707ea2009c164ae8786e3f42e181783
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2018-03-16 09:32:07 -0300

    add card

--- react/src/js/app.jsx
+++ react/src/js/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Title from './title/title';
 import Navbar from './navbar/navbar';
+import Card from './card/card';
 
 let app = (
   <div>
@@ -10,6 +11,17 @@ let app = (
     <div className="container">
       <Title />
       <h1>Ola Mundo</h1>
+      <div className="row">
+        <div className = "col m4">
+          <Card />
+        </div>
+        <div className = "col m4">
+          <Card />
+        </div>
+        <div className = "col m4">
+          <Card />
+        </div>
+      </div>
     </div>
   </div>
 );
